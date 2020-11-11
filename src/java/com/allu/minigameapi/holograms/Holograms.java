f8366dcab4f56529000e830e13d7831ae6e15319
Allu101 <a.lahtinen101@gmail.com>
2020-11-11 18:14:01 +0200

    Added void createHologramToPlayer() method

--- src/java/com/allu/minigameapi/holograms/Holograms.java
+++ src/java/com/allu/minigameapi/holograms/Holograms.java
@@ -14,6 +14,11 @@ public class Holograms {
         return hologram;
     }
 
+    public static void createHologramNotReturn(Location loc, String text) {
+        Hologram hologram = HologramsAPI.createHologram(MinigameAPI.plugin, loc);
+        hologram.insertTextLine(0, text);
+    }
+
     public static void createHologramToPlayer(Player p, Location loc, String text) {
         Hologram hologram = createHologram(loc, text);
         hologram.getVisibilityManager().setVisibleByDefault(false);
