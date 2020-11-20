d3df10abda875646ff593bf76f7253c819e0135e
Allu101 <a.lahtinen101@gmail.com>
2020-11-20 21:22:19 +0200

    Added new sendTitleAndSubTitle to TitleHandler.java

--- src/java/com/allu/minigameapi/player/TitleHandler.java
+++ src/java/com/allu/minigameapi/player/TitleHandler.java
@@ -8,12 +8,16 @@ public class TitleHandler {
     public TitleHandler() {
     }
 
+    public void sendSubTitle(Player p, String title) {
+        new Title("", ChatColor.translateAlternateColorCodes('&', title), 0, 30, 20).send(p);
+    }
+
     public void sendTitle(Player p, String title) {
         new Title(ChatColor.translateAlternateColorCodes('&', title), "", 0, 30, 20).send(p);
     }
 
-    public void sendSubTitle(Player p, String title) {
-        new Title("", ChatColor.translateAlternateColorCodes('&', title), 0, 30, 20).send(p);
+    public void sendTitleAndSubTitle(Player p, String title, String subtitle) {
+        new Title(ChatColor.translateAlternateColorCodes('&', title), subtitle, 0, 30, 20).send(p);
     }
 
 }
